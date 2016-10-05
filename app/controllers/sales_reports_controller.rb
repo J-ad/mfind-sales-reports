3e028e56628d31622a757a5044c95164b7a64488
Jan Adamski <jan.adamski@gmail.com>
2016-10-05 19:49:58 +0200

    WIP import filex to database

---
+++ app/controllers/sales_reports_controller.rb
@@ -0,0 +1,5 @@
+class SalesReportsController < ApplicationController
+  # http_basic_authenticate_with name: 'ipadmini', password: 'miniipad'
+  def index
+  end
+end
--- app/models/sales_report.rb
+++ app/models/sales_report.rb
@@ -1,3 +1,24 @@
 class SalesReport < ApplicationRecord
   belongs_to :partner
+
+  def self.import(file)
+    binding.pry
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      sale = find_by_id(row["id"]) || new
+      sale.attributes = row.to_hash.slice(*accessible_attributes)
+      sale.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".csv" then Roo::Csv.new(file.path)
+    when ".xls" then Roo::Excel.new(file.path)
+    when ".xlsx" then Roo::Excelx.new(file.path)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+end
 end
